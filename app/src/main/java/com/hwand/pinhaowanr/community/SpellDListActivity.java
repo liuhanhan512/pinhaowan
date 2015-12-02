9fca59dcf72a8520f168d49d131de02b4104be5f
shiqi <zengchan.lzc@alibaba-inc.com>
2015-12-02 08:09:37 +0800

    add file

--- app/src/main/java/com/hwand/pinhaowanr/community/SpellDListActivity.java
+++ app/src/main/java/com/hwand/pinhaowanr/community/SpellDListActivity.java
@@ -3,15 +3,27 @@ package com.hwand.pinhaowanr.community;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.hwand.pinhaowanr.BaseActivity;
 import com.hwand.pinhaowanr.R;
+import com.hwand.pinhaowanr.widget.SwipeRefreshLayout;
 
 /**
  * 某个课程的拼课列表
  * Created by hanhanliu on 15/12/2.
  */
-public class SpellDListActivity extends BaseActivity {
+public class SpellDListActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener{
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private ListView mListView;
+
+    private Adapter mAdapter;
 
     public static void launch(Context context){
         Intent intent = new Intent();
@@ -23,5 +35,65 @@ public class SpellDListActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spell_d_list_layout);
+
+        initTitle();
+        initViews();
+    }
+
+    private void initTitle(){
+
+    }
+
+    private void initViews(){
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        //加载颜色是循环播放的，只要没有完成刷新就会一直循环，color1>color2>color3>color4
+        mSwipeRefreshLayout.setColorScheme(android.R.color.white, android.R.color.holo_green_light,
+                android.R.color.holo_orange_light, android.R.color.holo_red_light);
+
+        mListView = (ListView) findViewById(R.id.listview);
+//        mListView.addHeaderView(initHeaderView());
+
+        mAdapter = new Adapter();
+        mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(mOnItemClickListener);
+    }
+
+    final AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        }
+    };
+
+    @Override
+    public void onRefresh() {
+
+    }
+
+    class Adapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            if(convertView == null){
+                convertView = View.inflate(SpellDListActivity.this , R.layout.spell_d_list_item_layout , null);
+            }
+            return convertView;
+        }
     }
 }
