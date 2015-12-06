81a14639a7c00a03b4000b1670944ad2b069fc44
shiqi <zengchan.lzc@alibaba-inc.com>
2015-12-06 17:47:46 +0800

    add file

--- app/src/main/java/com/hwand/pinhaowanr/community/SpellDFragment.java
+++ app/src/main/java/com/hwand/pinhaowanr/community/SpellDFragment.java
@@ -2,22 +2,38 @@ package com.hwand.pinhaowanr.community;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.google.gson.Gson;
 import com.hwand.pinhaowanr.BaseFragment;
+import com.hwand.pinhaowanr.CommonViewHolder;
 import com.hwand.pinhaowanr.MainApplication;
 import com.hwand.pinhaowanr.R;
 import com.hwand.pinhaowanr.model.HomePageModel;
+import com.hwand.pinhaowanr.model.SpellDCategoryModel;
+import com.hwand.pinhaowanr.model.SpellDEntity;
+import com.hwand.pinhaowanr.model.SpellDModel;
+import com.hwand.pinhaowanr.utils.AndTools;
 import com.hwand.pinhaowanr.utils.NetworkRequest;
 import com.hwand.pinhaowanr.utils.UrlConfig;
+import com.hwand.pinhaowanr.widget.SubGridView;
+import com.hwand.pinhaowanr.widget.SubListView;
 import com.hwand.pinhaowanr.widget.SwipeRefreshLayout;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,9 +46,18 @@ public class SpellDFragment extends BaseFragment implements SwipeRefreshLayout.O
 
     private SwipeRefreshLayout mSwipeRefreshLayout;
 
-    private ListView mListView;
+    private SubListView mListView;
 
     private Adapter mAdapter;
+
+    private LinearLayout mCategoryLayout;
+
+    private SubGridView mGridView;
+
+    private GridAdapter mGridAdapter;
+
+    private List<SpellDCategoryModel> spellDCategoryModels = new ArrayList<SpellDCategoryModel>();
+    private List<SpellDModel> spellDModels = new ArrayList<SpellDModel>();
 
     public static BaseFragment newInstance(){
         SpellDFragment fragment = new SpellDFragment();
@@ -55,12 +80,31 @@ public class SpellDFragment extends BaseFragment implements SwipeRefreshLayout.O
         mSwipeRefreshLayout.setColorScheme(android.R.color.white, android.R.color.holo_green_light,
                 android.R.color.holo_orange_light, android.R.color.holo_red_light);
 
-        mListView = (ListView)mFragmentView . findViewById(R.id.listview);
-        mListView.addHeaderView(initHeaderView());
+        mListView = (SubListView)mFragmentView . findViewById(R.id.listview);
+//        mListView.addHeaderView(initHeaderView());
 
         mAdapter = new Adapter();
         mListView.setAdapter(mAdapter);
         mListView.setOnItemClickListener(mOnItemClickListener);
+
+        mGridView = (SubGridView) mFragmentView.findViewById(R.id.category_gird);
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int categorySize = spellDCategoryModels.size();
+                for (int i= 0 ; i < categorySize ; i++){
+                    SpellDCategoryModel spellDCategoryModel = spellDCategoryModels.get(i);
+                    if(i == position){
+                        spellDCategoryModel.setIsSelected(true);
+                    } else {
+                        spellDCategoryModel.setIsSelected(false);
+                    }
+                }
+                mGridAdapter.notifyDataSetChanged();
+            }
+        });
+        mGridAdapter = new GridAdapter();
+        mGridView.setAdapter(mGridAdapter);
 
         fetchData();
     }
@@ -87,6 +131,16 @@ public class SpellDFragment extends BaseFragment implements SwipeRefreshLayout.O
             public void onResponse(String response) {
                 mSwipeRefreshLayout.setRefreshing(false);
                 if (!TextUtils.isEmpty(response)) {
+                    Gson gson = new Gson();
+                    SpellDEntity spellDEntity = gson.fromJson(response , SpellDEntity.class);
+                    if(spellDEntity != null){
+
+                        List<SpellDCategoryModel> spellDCategoryModels = spellDEntity.getClassTypeList();
+                        wrapperSpellDCategoryData(spellDCategoryModels);
+
+                        List<SpellDModel> spellDModels = spellDEntity.getPinClassList();
+                        updatePinView(spellDModels);
+                    }
 
                 }
 
@@ -99,6 +153,28 @@ public class SpellDFragment extends BaseFragment implements SwipeRefreshLayout.O
         });
     }
 
+    private void wrapperSpellDCategoryData(List<SpellDCategoryModel> spellDCategoryModels){
+        if(spellDCategoryModels != null){
+            SpellDCategoryModel all = new SpellDCategoryModel();
+            all.setClassName(getString(R.string.all));
+            all.setClassType(-1);
+            all.setIsSelected(true);
+            spellDCategoryModels.add(0, all);
+
+            this.spellDCategoryModels.clear();
+            this.spellDCategoryModels.addAll(spellDCategoryModels);
+            mGridAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void updatePinView(List<SpellDModel> spellDModels){
+        if(spellDModels != null){
+            this.spellDModels.clear();
+            this.spellDModels.addAll(spellDModels);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
     @Override
     public void onRefresh() {
         fetchData();
@@ -108,7 +184,7 @@ public class SpellDFragment extends BaseFragment implements SwipeRefreshLayout.O
 
         @Override
         public int getCount() {
-            return 0;
+            return spellDModels.size();
         }
 
         @Override
@@ -126,6 +202,65 @@ public class SpellDFragment extends BaseFragment implements SwipeRefreshLayout.O
             if(convertView == null){
                 convertView = View.inflate(getActivity() , R.layout.spell_d_list_item_layout , null);
             }
+            SpellDModel spellDModel = spellDModels.get(position);
+
+            RelativeLayout imageLayout = CommonViewHolder.get(convertView , R.id.image_layout);
+            int screenWidth = AndTools.getScreenWidth(getActivity());
+            int height = screenWidth * 9 / 16;
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) imageLayout.getLayoutParams();
+            layoutParams.height = height;
+            imageLayout.setLayoutParams(layoutParams);
+
+            ImageView imageView = CommonViewHolder.get(convertView, R.id.image);
+            AndTools.displayImage(null, spellDModel.getPictureUrl(), imageView);
+
+            TextView description = CommonViewHolder.get(convertView , R.id.description);
+            description.setText(spellDModel.getSubtitle());
+
+            TextView className = CommonViewHolder.get(convertView , R.id.class_name);
+            className.setText(spellDModel.getClassName());
+
+            TextView address = CommonViewHolder.get(convertView , R.id.address);
+            address.setText(spellDModel.getDetailAddress());
+            return convertView;
+        }
+    }
+
+    class GridAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return spellDCategoryModels.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                convertView = View.inflate(getActivity() , R.layout.spell_d_category_item_layout ,null);
+            }
+
+            SpellDCategoryModel spellDCategoryModel = spellDCategoryModels.get(position);
+            TextView category = CommonViewHolder.get(convertView , R.id.category_text);
+            category.setText(spellDCategoryModel.getClassName());
+            category.setSelected(spellDCategoryModel.isSelected());
+
+            RelativeLayout categoryLayout = CommonViewHolder.get(convertView , R.id.category_layout);
+            if(spellDCategoryModel.isSelected()){
+                categoryLayout.setBackgroundResource(R.drawable.red_solid_corner_bg);
+            } else {
+                categoryLayout.setBackgroundResource(R.drawable.red_corner_bg);
+            }
+
             return convertView;
         }
     }
