24acb3512f4470ccb7267d983ada7cab7fb9d23c
shiqi <zengchan.lzc@alibaba-inc.com>
2015-12-07 22:59:23 +0800

    add file

---
+++ app/src/main/java/com/hwand/pinhaowanr/community/BaseCommunityFragment.java
@@ -0,0 +1,17 @@
+package com.hwand.pinhaowanr.community;
+
+import com.hwand.pinhaowanr.BaseFragment;
+
+/**
+ * Created by hanhanliu on 15/12/7.
+ */
+public class BaseCommunityFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    public void fetchData(){
+
+    }
+}
